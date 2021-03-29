11cb2ce7d104f4c32510f924b4d9e562624790d9
aksmith5239 <aklobby@gmail.com>
2021-03-29 17:42:12 -0600

    sms send setup completed

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
-const twilio = require('twilio');
+// const socketio = require('socket.io');
+const http = require('http');
+const MessagingResponse =require('twilio').twiml.MessagingResponse;
+
+const accountSid = 'AC2371c2fffdbeddb5c970f4fc087dd212';
+const authToken = '21eb7cbb19753b69807f75ca78760c8e';
+
+const client = require('twilio')(accountSid, authToken);
+
 
 //init to express
 const app = express();
@@ -20,7 +28,41 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
     res.render('index');
 })
-//port definition
-const port = 3000;
+//post -  form submit
+app.post('/', (req, res) => {
+    const number = req.body.number;
+    const text = req.body.text;
+    
+    client.messages.create({
+        to: number, 
+        from: '+15052078535', 
+        body: text
+       })
+       .then((message) => console.log(message.sid))
+});
 
-const server = app.listen(port, () => console.log(`🌎 Server started on port ${port}`));
+app.post('/sms', (req, res) => {
+    const twiml = new MessagingResponse();
+
+    twiml.message('We got it.');
+
+    res.writeHead(200, {'ContentType': 'text/xml'});
+    res.end(twiml.toString());
+});
+
+
+//port definition
+// const port = 1337;
+
+// const server = app.listen(port, () => console.log(`🌎 Server started on port ${port}`));
+
+http.createServer(app).listen(1337, () => {
+    console.log(`Server listening` );
+})
+// const io = socketio(server);
+// io.on('connection', (socket) => {
+//     console.log('Connected');
+//     io.on('disconnect', () => {
+//         console.log('Disconnected');
+//     })
+// })
